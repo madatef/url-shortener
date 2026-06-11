9233a31256253ef031c184ad4d06879fe1b3fe24
madatef <madatef1@outlook.com>
2026-06-11 08:04:09 +0300

    feat(settings): add DB and auth env vars

--- app/config.py
+++ app/config.py
@@ -4,6 +4,12 @@ class Settings(BaseSettings):
     DB_HOST: str = 'localhost'
     DB_PORT: int = 5432
     DB_USER: str = 'postgres'
-    DB_PASSWORD: str = '3734'
+    DB_PASSWORD: str
+
+    SECRET_KEY: str
+    ALGORITHM: str
+
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
